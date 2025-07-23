d60d0e051e9291dc57d40f30dc18e8a457e06e4a
Not only <2761404661@qq.com>
2025-07-24 00:47:06 +0800

    Add files via upload

---
+++ 2025测绘程序设计-泰森多边形/泰森多边形7.23/Triangel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 泰森多边形7._23
+{
+    internal class Triangel
+    {
+        public string id;
+        public MyPoint p1;
+        public MyPoint p2;
+        public MyPoint p3;
+        public Triangel()
+        {
+
+        }
+        public Triangel(string id, MyPoint p1, MyPoint p2, MyPoint p3)
+        {
+            this.id = id;
+            this.p1 = p1;
+            this.p2 = p2;
+            this.p3 = p3;
+        }
+    }
+}
